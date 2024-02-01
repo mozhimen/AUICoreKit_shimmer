2c105fa01f24ec0309649ce3611e57a0e48e318a
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-02-01 22:37:16 +0800

    0201

--- sample/src/main/java/com/facebook/shimmer/sample/MainActivity.kt
+++ sample/src/main/java/com/facebook/shimmer/sample/MainActivity.kt
@@ -9,34 +9,32 @@
 package com.facebook.shimmer.sample
 
 import android.animation.ValueAnimator
-import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerFrameLayout
-import kotlinx.android.synthetic.main.main.*
+import com.facebook.shimmer.sample.databinding.MainBinding
+import com.mozhimen.basick.elemk.androidx.appcompat.bases.databinding.BaseActivityVB
 
-class MainActivity : Activity(), View.OnClickListener {
+class MainActivity : BaseActivityVB<MainBinding>(), View.OnClickListener {
   private lateinit var shimmerViewContainer: ShimmerFrameLayout
   private lateinit var presetButtons: Array<Button>
   private var currentPreset = -1
   private var toast: Toast? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main)
-    shimmerViewContainer = shimmer_view_container
+  override fun initView(savedInstanceState: Bundle?) {
+    shimmerViewContainer = vb.shimmerViewContainer
     presetButtons =
         arrayOf(
-            preset_button0,
-            preset_button1,
-            preset_button2,
-            preset_button3,
-            preset_button4,
-            preset_button5,
-            preset_button6)
+            vb.presetButton0,
+            vb.presetButton1,
+            vb.presetButton2,
+            vb.presetButton3,
+            vb.presetButton4,
+            vb.presetButton5,
+            vb.presetButton6)
     presetButtons.forEach { it.setOnClickListener(this@MainActivity) }
     selectPreset(0, false)
   }
@@ -77,16 +75,19 @@ class MainActivity : Activity(), View.OnClickListener {
             toast = Toast.makeText(this, "Slow and reverse", Toast.LENGTH_SHORT)
             shimmerBuilder.setDuration(5000L).setRepeatMode(ValueAnimator.REVERSE)
           }
+
           2 -> {
             // Thin, straight and transparent
             toast = Toast.makeText(this, "Thin, straight and transparent", Toast.LENGTH_SHORT)
             shimmerBuilder.setBaseAlpha(0.1f).setDropoff(0.1f).setTilt(0f)
           }
+
           3 -> {
             // Sweep angle 90
             toast = Toast.makeText(this, "Sweep angle 90", Toast.LENGTH_SHORT)
             shimmerBuilder.setDirection(Shimmer.Direction.TOP_TO_BOTTOM).setTilt(0f)
           }
+
           4 -> {
             // Spotlight
             toast = Toast.makeText(this, "Spotlight", Toast.LENGTH_SHORT)
@@ -97,6 +98,7 @@ class MainActivity : Activity(), View.OnClickListener {
                 .setIntensity(0.35f)
                 .setShape(Shimmer.Shape.RADIAL)
           }
+
           5 -> {
             // Spotlight angle 45
             toast = Toast.makeText(this, "Spotlight angle 45", Toast.LENGTH_SHORT)
@@ -108,11 +110,13 @@ class MainActivity : Activity(), View.OnClickListener {
                 .setTilt(45f)
                 .setShape(Shimmer.Shape.RADIAL)
           }
+
           6 -> {
             // Off
             toast = Toast.makeText(this, "Off", Toast.LENGTH_SHORT)
             null
           }
+
           else -> {
             toast = Toast.makeText(this, "Default", Toast.LENGTH_SHORT)
             shimmerBuilder
